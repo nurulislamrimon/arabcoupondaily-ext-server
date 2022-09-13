79f722b6d9fe210bdf5b9fb263c78bc1bb4a008b
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-09-13 22:15:37 +0600

    post and delete added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -19,11 +19,29 @@ async function run() {
     try {
         const couponCollection = client.db('couponCollection').collection('coupon')
 
+
+        // get all coupon
         app.get('/coupons', async (req, res) => {
             const query = {};
             const result = await couponCollection.find(query).toArray();
             res.send(result);
             console.log('all coupons response');
+        });
+        // add new coupon
+        app.post('/coupon', async (req, res) => {
+            const newCoupon = req.body;
+            const result = await couponCollection.insertOne(newCoupon);
+            res.send(result);
+            console.log('new coupon added');
+        })
+        // delete coupon
+        app.delete('/coupon/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await couponCollection.deleteOne(query);
+            res.send(result);
+            console.log(`${id} is deleted`);
+
         })
 
         app.get('/', (req, res) => {
